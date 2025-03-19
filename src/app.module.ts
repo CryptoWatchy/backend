4c95706b06142701e3c7adaf529c4318b4c79f35
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-19 18:54:51 +0100

    db connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common'
-import { TokenModule } from './token/token.module';
+import { TokenModule } from './token/token.module'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [TokenModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TokenModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
 })
 export class AppModule {}
