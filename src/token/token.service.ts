0c020c5a93f5c37110b4eabfc122ca011b857cb4
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-19 18:55:40 +0100

    service functions implemented for get, patch and post endpoints

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,4 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Token, TokenTx, TokenUnit } from './entities'
+import { UpdateFavoriteDto, CreateTokenTxDto } from './dto'
 
 @Injectable()
-export class TokenService {}
+export class TokenService {
+  constructor(
+    @InjectRepository(Token) private tokenRepository: Repository<Token>,
+    @InjectRepository(TokenTx) private tokenTxRepository: Repository<TokenTx>,
+  ) {
+    console.log('Hi!: ', TokenUnit)
+  }
+
+  async getAllTokens() {
+    return this.tokenRepository.find()
+  }
+
+  async getFavoriteStatus(unit: TokenUnit) {
+    const token = await this.tokenRepository.findOne({ where: { unit } })
+
+    return token ? token.favorite : null
+  }
+
+  async updateFavoriteStatus(
+    unit: TokenUnit,
+    updateFavoriteDto: UpdateFavoriteDto,
+  ) {
+    await this.tokenRepository.update(
+      { unit },
+      { favorite: updateFavoriteDto.favorite },
+    )
+
+    return { message: 'Favorite status updated' }
+  }
+
+  async saveTokenTransaction(
+    unit: TokenUnit,
+    createTokenTxDto: CreateTokenTxDto,
+  ) {
+    const token = await this.tokenRepository.findOne({ where: { unit } })
+    if (!token) throw new Error('Token not found')
+
+    token.amount += createTokenTxDto.amount
+    token.value = token.amount * token.price
+    await this.tokenRepository.save(token)
+
+    const tx = this.tokenTxRepository.create({
+      token,
+      tokenUnit: unit,
+      amount: createTokenTxDto.amount,
+      price: token.price,
+      comment: createTokenTxDto.comment,
+    })
+    await this.tokenTxRepository.save(tx)
+
+    return { message: 'Transaction saved' }
+  }
+
+  async getTokenTransactions(unit: TokenUnit) {
+    return this.tokenTxRepository.find({
+      where: { tokenUnit: unit },
+      order: { createdAt: 'DESC' },
+    })
+  }
+
+  async getTotalValue() {
+    const tokens = await this.tokenRepository.find()
+
+    return tokens.reduce((sum, token) => sum + token.value, 0)
+  }
+}
