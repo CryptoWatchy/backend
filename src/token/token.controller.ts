4eb1a7f90cd0c08ef633fa4965fdd523a25e7ac3
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-19 18:55:56 +0100

    controllers implemented for get, patch and post endpoints

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch } from '@nestjs/common'
+import { TokenService } from './token.service'
+import { TokenUnit } from './entities'
+import { UpdateFavoriteDto, CreateTokenTxDto } from './dto'
 
 @Controller('token')
-export class TokenController {}
+export class TokenController {
+  constructor(private readonly tokenService: TokenService) {}
+
+  @Get()
+  getAllTokens() {
+    return this.tokenService.getAllTokens()
+  }
+
+  @Get(':unit/favorite')
+  getFavoriteStatus(@Param('unit') unit: TokenUnit) {
+    return this.tokenService.getFavoriteStatus(unit)
+  }
+
+  @Patch(':unit/favorite')
+  updateFavoriteStatus(
+    @Param('unit') unit: TokenUnit,
+    @Body() updateFavoriteDto: UpdateFavoriteDto,
+  ) {
+    return this.tokenService.updateFavoriteStatus(unit, updateFavoriteDto)
+  }
+
+  @Post(':unit')
+  saveTokenTransaction(
+    @Param('unit') unit: TokenUnit,
+    @Body() createTokenTxDto: CreateTokenTxDto,
+  ) {
+    return this.tokenService.saveTokenTransaction(unit, createTokenTxDto)
+  }
+
+  @Get(':unit/comments')
+  getTokenTransactions(@Param('unit') unit: TokenUnit) {
+    return this.tokenService.getTokenTransactions(unit)
+  }
+
+  @Get('total-value')
+  getTotalValue() {
+    return this.tokenService.getTotalValue()
+  }
+}
