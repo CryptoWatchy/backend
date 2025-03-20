191a02fd5fef97df56b8602c3838ef4a67451f41
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-20 13:29:00 +0100

    dtos updated to validate proper var type

--- src/token/dto/create-token-tx.dto.ts
+++ src/token/dto/create-token-tx.dto.ts
@@ -1,4 +1,10 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator'
+
 export class CreateTokenTxDto {
+  @IsNumber()
   amount: number
+
+  @IsOptional()
+  @IsString()
   comment?: string
 }
--- src/token/dto/update-favorite.dto.ts
+++ src/token/dto/update-favorite.dto.ts
@@ -1,3 +1,6 @@
+import { IsBoolean } from 'class-validator'
+
 export class UpdateFavoriteDto {
+  @IsBoolean()
   favorite: boolean
 }
