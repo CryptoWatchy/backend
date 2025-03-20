f2723d529bfb4cb516321405dc3b76bee63b82b8
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-20 13:30:54 +0100

    registered HttpModule and TokenPriceService

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -3,10 +3,12 @@ import { TokenController } from './token.controller'
 import { TokenService } from './token.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Token, TokenTx } from './entities'
+import { TokenPriceService } from './token-price.service'
+import { HttpModule } from '@nestjs/axios'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Token, TokenTx])],
+  imports: [TypeOrmModule.forFeature([Token, TokenTx]), HttpModule],
   controllers: [TokenController],
-  providers: [TokenService],
+  providers: [TokenService, TokenPriceService],
 })
 export class TokenModule {}
